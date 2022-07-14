ad6bf2b7aab54aa86a609c7dc28d63d67ad8f6fa
Seditio CMS <bilgi@ntka.org>
2022-07-14 10:05:44 +0300

    Forum Topics Search

---
+++ ftsearch/lang/ftsearch.tr.lang.php
@@ -0,0 +1,24 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditiocms.com
+[BEGIN_SED]
+File=plugins/ftsearch/lang/ftsearch.tr.lang.php
+Version=178
+Updated=2022-jul-07
+Type=
+Author=Neocrome
+Description=
+[END_SED]
+==================== */
+
+$L['ftsearch'] = "Okunmamış Mesajlar";
+$L['getdaily'] = "Bugünün Yazıları";
+$L['unanswered'] = "Cevaplanmamış konular";
+$L['error_case_getnew'] = "Üzgünüz, son ziyaretinizden bu yana yeni konu yok.";
+$L['error_case_getdaily'] = "Üzgünüz, son 24 saat içinde yeni konu yok.";
+$L['error_case_unanswered'] = "Cevapsız konu olmadığı için çok mutluyuz.";
+
+?>
