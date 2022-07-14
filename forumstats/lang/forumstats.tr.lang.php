d4492b4720ff6ae841b0281c75e696a0234189e4
Seditio CMS <bilgi@ntka.org>
2022-07-14 10:02:45 +0300

    Statistics for the forums

---
+++ forumstats/lang/forumstats.tr.lang.php
@@ -0,0 +1,29 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome
+http://www.neocrome.net
+[BEGIN_SED]
+File=plugins/lang/forumstats.tr.lang.php
+Version=178
+Updated=2020-jul-07
+Type=
+Author=Neocrome
+Description=
+[END_SED]
+==================== */
+
+
+$L['plu_title'] = "Forumlar için istatistikler";
+
+$L['plu_posts'] = "Toplam gönderiler";
+$L['plu_topics'] = "Toplam konular";
+$L['plu_sections'] = "Toplam bölümler";
+$L['plu_views'] = "Toplam görüntülenme";
+$L['plu_posterstop10'] = "Posterler En İyi 10";
+$L['plu_viewedtop10'] = "En çok görüntülenen 10 konu";
+$L['plu_repliedtop10'] = "en çok cevaplanan 10 konu";
+$L['plu_hidden'] = "(gizli, erişime izin verilmiyor)";
+
+?>
