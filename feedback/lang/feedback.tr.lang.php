9d62d11bd4e1a2f24a066cc3c54e48eb6bc2be9d
Seditio CMS <bilgi@ntka.org>
2024-01-09 13:11:04 +0300

    Create feedback.tr.lang.php

---
+++ feedback/lang/feedback.tr.lang.php
@@ -0,0 +1,38 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditio.com.tr
+[BEGIN_SED]
+File=plugins/feedback/lang/feedback.tr.lang.php
+Version=179
+Updated=2022-jul-15
+Type=
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['plu_title'] = "Geri bildirim";
+
+$L['plu_explain'] = "Bize e-posta göndermek için bu formu doldurun, en kısa sürede yanıt vereceğiz !";
+$L['plu_recipients_title'] = "Alıcı";
+$L['plu_email_title'] = "Email";
+$L['plu_name_title'] = "İsim";
+$L['plu_phone_title'] = "Telefon numarası";
+$L['plu_subject_title'] = "Konu";
+$L['plu_message_title'] = "Mesaj";
+$L['plu_attach'] = "Dosya ekle";
+$L['plu_required'] = "* = Gerekli";
+$L['plu_verify'] = "İnsan olduğunuzdan emin olmak için lütfen bu numarayı noktalar olmadan girin : ";
+$L['plu_send'] = "Gönder";
+
+$L['plu_fieldempty'] = "En az bir alan boş.";
+$L['plu_wrongentry'] = "En az bir alanda hata var.";
+$L['plu_antispam'] = "Spambot koruma anahtarı hatalı, lütfen yeniden yazın !";
+$L['plu_notsent'] = "Mesaj GÖNDERİLMEDİ.";
+$L['plu_sent'] = "Mesaj başarıyla iletildi !";
+$L['plu_notice'] = "Bu mesaj şu adresten gönderildi: ".(isset($cfg['maintitle'])?$cfg['maintitle']:"")." by : ";
+
+?>
